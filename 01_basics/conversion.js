85a23f6f415b7708db20ce2c3e8737ab2af31d52
SOUMYA <109866522+poddar99@users.noreply.github.com>
2025-12-14 06:11:38 +0000

    data summery

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -24,5 +24,23 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
- console.log(stringNumber);
- console.log(typeof stringNumber);
+ //console.log(stringNumber);
+ //console.log(typeof stringNumber);
+
+ //.....................operations.................
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+
+console.log(((3+4) *5) %3);
+ console.log(+true);
+ console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
